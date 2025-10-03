c79bea97fb40f29ac9c2e6d0bd25fb464b694d88
Username4D <studio@kremzow.org>
2025-10-03 12:26:44 +0200

    create physics playground scene, create coin object, created custom physics to make playing with coin better

---
+++ scenes/coin.gd
@@ -0,0 +1,10 @@
+extends RigidBody2D
+
+@onready var ideal_gravity = 0
+
+func _physics_process(delta: float) -> void:
+	ideal_gravity = clamp(1 + linear_velocity.y / 500, 0.2, 3)
+	self.gravity_scale = move_toward(self.gravity_scale, ideal_gravity, 0.08)
+	#self.mass = 1 + linear_velocity.y / 30
+	print(gravity_scale)
+	
