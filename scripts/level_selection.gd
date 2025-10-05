7b83089a65981c5cbf7f12a1b947c897df203faa
Username4D <studio@kremzow.org>
2025-10-05 20:43:44 +0200

    added loading savefile to level menu

--- scripts/level_selection.gd
+++ scripts/level_selection.gd
@@ -1,16 +1,35 @@
 extends HBoxContainer
 
 var max_level = 7
-var stars = [true,false,true,true,false,false,true,true]
+var stars = []
 func _ready() -> void:
+	var file = FileAccess.open("user://levels.dat", FileAccess.READ)
+	if file == null:
+		file = FileAccess.open("user://levels.dat", FileAccess.WRITE)
+		file.store_var([0,0,0,0,0,0,0,0])
+		stars = [false,false,false,false,false,false,false,false]
+		max_level = 0
+	else:
+		
+		var array = file.get_var()
+		print(array)
+		for i in array:
+			if i == 2:
+				stars.append(true)
+			else:
+				stars.append(false)
+		max_level = array.find(0)
+		print(max_level)
 	for i in get_children():
 		i.get_node("label").text = i.name
 		if int(i.name) -1 > max_level:
 			i.get_node("label").visible = false
 			i.self_modulate.a = 0.5
+		else:
+			i.pressed.connect(start_level.bind(i.name))
 		if stars[int(i.name) -1 ]:
-			i.get_node("star").visible = false
-		i.pressed.connect(start_level.bind(i.name))
+			i.get_node("star").visible = true
+		
 	
 func start_level(lvl): 
 	var gp_scene = load("res://scenes/gameplay_scene.tscn").instantiate()
