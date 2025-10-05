1dea512ca9d84ce880a3ab169ac6302d338eb6f5
Username4D <studio@kremzow.org>
2025-10-05 21:41:39 +0200

    added lvl 2

--- scripts/d_well.gd
+++ scripts/d_well.gd
@@ -1,6 +1,18 @@
 extends Node2D
 
-
-
 func _on_play_pressed() -> void:
-	pass # Replace with function body.
+	var new = load("res://scenes/menu_level.tscn").instantiate()
+	self.get_parent().add_child(new)
+	self.queue_free()
+
+func _on_settings_pressed() -> void:
+	$settings_popup.visible = true
+
+func _on_button_pressed() -> void:
+	$settings_popup.visible = false
+
+func _on_credits_pressed() -> void:
+	$credits_popup.visible = true
+
+func _on_button2_pressed() -> void:
+	$credits_popup.visible = false
